9428b36b2f86f8d1ebb6629d260277d0992b7496
Sirapope Panapong <sirapope@profile.co.th>
2022-12-30 23:57:08 +0700

    Add project files.

---
+++ bsLogin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfBookStores
+{
+    /// <summary>
+    /// Interaction logic for bsLogin.xaml
+    /// </summary>
+    public partial class bsLogin : Window
+    {
+        public bsLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
